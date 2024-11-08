dabacbf99c5452513311d35ff805276213e01682
Jenna Salmela <jlesalmela@gmail.com>
2024-11-08 11:52:28 +0200

    Joukkoon livahtaneen build-kansion poistoyritys

--- code/build/image_preload.cmake
+++
@@ -1,6 +0,0 @@
-# Generated file that can be used to preload variant images
-set(BOARD "nrf5340dk/nrf5340/cpuapp/ns" CACHE INTERNAL "NCS child image controlled")
-set(CONF_FILE_BUILD_TYPE "" CACHE INTERNAL "NCS child image controlled")
-set(DTC_OVERLAY_FILE "C:/nordic/myapps/tlprojekti_g13/code/nrf5340dk_nrf5340_cpuapp_ns.overlay" CACHE INTERNAL "NCS child image controlled")
-set(NCS_TOOLCHAIN_VERSION "NONE" CACHE INTERNAL "NCS child image controlled")
-set(WEST_PYTHON "C:/ncs/toolchains/ce3b5ff664/opt/bin/python.exe" CACHE INTERNAL "NCS child image controlled")
