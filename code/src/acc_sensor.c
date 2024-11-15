b8d6856801ceb1a72ea9026f7cbbff40dd7643fb
Jenna Salmela <jlesalmela@gmail.com>
2024-11-15 10:05:33 +0200

    Jotain ajuri-ihmettelyä

--- code/src/acc_sensor.c
+++ code/src/acc_sensor.c
@@ -1,1 +1,48 @@
 #include "acc_sensor.h"
+
+#define ACCELEROMETER DT_ALIAS(accel)
+#if DT_NODE_HAS_STATUS(ACCELEROMETER, okay)
+const struct device *const acc_device =  DEVICE_DT_GET(ACCELEROMETER);
+#else
+#error "No accelerometer node"
+#endif
+
+
+
+#define CHANNEL_NO 3
+
+int test(void) {
+    printk("test");
+    
+    return 0;
+}
+/*
+int read_accelerometer(const struct  *device, struct AccelerationData *data)
+{
+    int result = 0;
+
+    result = sensor_sample_fetch(device);
+    if (result < 0) {
+        printk("Couldn't fetch sample, error %d.\n", result);
+        return result;
+    }
+    
+    struct sensor_value readings[3];
+    for (size_t i = 0; i < CHANNEL_NO; i++) {
+        result = sensor_channel_get(device, channels[i], &readings[i]);
+        if (result < 0) {
+            printk("Error with reading axis %d, error %d.\n", i, result);
+            return result;
+        }
+    }
+
+    double val_x, val_y, val_z;
+    val_x = sensor_value_to_double(&readings[0]);
+    val_y = sensor_value_to_double(&readings[1]);
+    val_z = sensor_value_to_double(&readings[2]);
+
+    printk("Values: x = %.2f, y = %.2f, z = %.2f.\n", val_x, val_y, val_z);
+
+    return 0;
+}
+*/
