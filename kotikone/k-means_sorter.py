7b6805b3c39d6c36995284d7a8b8a89784c07b36
Jenna Salmela <jlesalmela@gmail.com>
2024-11-28 19:47:17 +0200

    Poistettu vanha k-means-prototyyppi

--- kotikone/k-means_sorter.py
+++ kotikone/k-means_sorter.py
@@ -16,30 +16,72 @@ def randomize_centers(num, limits):
     return centers
 
 def calculate_k_means(points, centers):
-    #result = np.array([points[0:99], points[100:199], points[200:299] ,points[300:399], points[400:499], points[500:599]])
-    result = np.array((1,6))
+    # Muuttuja pitämään kirjaa, onko tällä kierroksella tapahtunut muutosta
+    finished = False
+    # Muuttuja pitämään kirjaa viime kierroksen voittajista
+    old_centers = centers
+    rng = np.random.default_rng()
 
-    # Määrät, keskietäisyydet
-    # Pisteiden lähiomaistiedot sisältävä lista?
-    for i in range(len(points)):
-        # Tuloksen laskennan valmistelu
-        for j in range(len(centers)):
-            x = 1
-        # Vau, tulos saatu pisteelle
-    # Lopputuloksen siirto listoihin lähiomaistiedon mukaan?
-    # Vau, tulos saatu tehtävään
-    return result
+    # Toista uusien keskipisteiden laskentaa, kunnes muutosta ei enää tapahdu
+    while (not finished) :
+        #Alustetaan tarvittavat listat
+        distances = np.zeros(len(old_centers))
+        centerPointCumulativeSum = np.zeros(shape=(6,3))
+        counts = np.zeros(len(old_centers))
+
+        #Käydään läpi kaikki datapisteet
+        for i in range(len(points)):
+            #Käydään läpi kaikki keskipisteet
+            for j in range(len(old_centers)):
+                #Lasketaan jokaisen datapisteen etäisyys kuhunkin keskipisteeseen
+                distances[j] = np.linalg.norm(old_centers[j] - points[i])
+
+            #Poimitaan pienimmän etäisyyden indeksi talteen
+            index = np.argmin(distances)
+            #Kasvatetaan counts-listan arvoa pienimmän etäisyyden indeksin kohdalta yhdellä
+            counts[index] += 1
+            #Lisätään voittajakeskipisteelle datapisteen arvot
+            centerPointCumulativeSum[index] += points[i]
+
+        #print(counts)
+        #print(centerPointCumulativeSum)
+
+        #Alustetaan taulukko uusille keskipisteille
+        new_centers = np.zeros_like(old_centers)
+        # Valmistellaan muutoksen tarkistus (epäonnistuminen estää)
+        finished = True
+        for i in range(len(old_centers)):
+            #Jos keskipiste voitti datapisteitä, lasketaan uusi keskipiste
+            if counts[i] > 0:
+                new_centers[i] = centerPointCumulativeSum[i] / counts[i]
+            #Jos keskipiste jäi tyhjäksi, arvotaan uusi keskipiste
+            elif counts[i] == 0:
+                new_centers[i] = rng.integers(1500, 2200, size=3)
+                # "Tämä ei jää tähän, minä voitan vielä itselleni pisteitä" - Uusi Keskipiste
+                finished = False
+            # Jos muutosta on tapahtunut, ohjelma ei lakkaa tähän kierrokseen
+            if (old_centers[i] != new_centers[i]).all():
+                finished = False
+            # Tee tämän kierroksen uusista pisteistä seuraavan kierroksen vanhat
+            old_centers[i] = new_centers[i]            
+        
+        #print(old_centers)
+        #print("uudet", new_centers)
+
+    return new_centers
 
 def plot_results(points, centers):
     fig = plot.figure()
     ax = fig.add_subplot(projection="3d")
-    print(points[0,:,0])
-    ax.scatter(points[0,:,0], points[0,:,1], points[0,:,2], c='b')
-    ax.scatter(points[1,:,0], points[1,:,1], points[1,:,2], c='g')
-    ax.scatter(points[2,:,0], points[2,:,1], points[2,:,2], c='m')
-    ax.scatter(points[3,:,0], points[3,:,1], points[3,:,2], c='y')
-    ax.scatter(points[4,:,0], points[4,:,1], points[4,:,2], c='r')
-    ax.scatter(points[5,:,0], points[5,:,1], points[5,:,2], c='c')
+    #print("Points:", len(points), len(points[0]), points[:,:])
+    # Datapisteet ovat meidän setissämme järjestyksessä, eli voimme laittaa ne pätkittäin
+    # tietyn väriseksi ja tietää, että ne ovat nätissä ryhmässä
+    ax.scatter(points[0:99,0], points[0:99,1], points[0:99,2], c='b')
+    ax.scatter(points[100:199,0], points[100:199,1], points[100:199,2], c='g')
+    ax.scatter(points[200:299,0], points[200:299,1], points[200:299,2], c='m')
+    ax.scatter(points[300:399,0], points[300:399,1], points[300:399,2], c='y')
+    ax.scatter(points[400:499,0], points[400:499,1], points[400:499,2], c='r')
+    ax.scatter(points[500:599,0], points[500:599,1], points[500:599,2], c='c')
     ax.scatter(centers[:,0], centers[:,1], centers[:,2], c='k')
     ax.set_xlabel("X axis")
     ax.set_ylabel("Y axis")
@@ -47,8 +89,9 @@ def plot_results(points, centers):
 
     plot.show()
 
+
 data = read_data()
 centers = randomize_centers(6, [[1500, 2200], [1500, 2200], [1500, 2200], [1500, 2200], [1500, 2200], [1500, 2200]])
 #print(centers)
 clusters = calculate_k_means(data, centers)
-plot_results(clusters, centers)
+plot_results(data, clusters)
