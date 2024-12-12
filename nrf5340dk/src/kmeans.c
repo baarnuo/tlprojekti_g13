67b42e4740c3f1d5b8a85ab3f148b8f1f60beed1
Jenna Salmela <jlesalmela@gmail.com>
2024-12-12 10:31:40 +0200

    K-means alkuun

---
+++ nrf5340dk/src/kmeans.c
@@ -0,0 +1,6 @@
+#include "kmeans.h"
+
+int16_t kmeans_direction(int16_t x, int16_t y, int16_t z)
+{
+    
+}
---
+++ nrf5340dk/src/kmeans.h
@@ -0,0 +1,6 @@
+#ifndef KMEANS_H
+#define KMEANS_H
+
+int16_t kmeans_direction(int16_t x, int16_t y, int16_t z);
+
+#endif
